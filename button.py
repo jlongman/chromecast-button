bd0738c05ab9abb55a4aa86c7cfdb5629207ea97
J. Longman <jlongman@gmail.com>
2017-04-02 03:24:57 +0000

    we discover the chromecast, be more consistent with path

--- button.py
+++ button.py
@@ -8,14 +8,31 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 alternate = False
+
+
+# scan for the chromecast and save it's logical address
+with open("/home/pi/chromecast-button/device", "w") as f:
+  device = 4
+  scan = subprocess.Popen("echo scan | cec-client -s -d 1", stdout=subprocess.PIPE, shell=True)
+  for line in scan.stdout:
+     line = line.lower()
+     if line.find("device #") > -1:
+          info = line.split()
+          device = info[1][1:-1]
+     if line.find("chromecast") > -1:
+          print(str(device))
+          break
+  f.write(device)
+  f.close
+
 while True:
 	input_state = GPIO.input(5)
 	if input_state == False:
 		if alternate:
-			p = subprocess.Popen("/home/pi/button/play.sh", shell=True, preexec_fn=os.setsid)
+			p = subprocess.Popen("/home/pi/chromecast-button/play.sh", shell=True, preexec_fn=os.setsid)
 			print("play")
 		else:	
-			p = subprocess.Popen("/home/pi/button/pause.sh", shell=True, preexec_fn=os.setsid)
+			p = subprocess.Popen("/home/pi/chromecast-button/pause.sh", shell=True, preexec_fn=os.setsid)
 			print("pause")
 		time.sleep(2)
 		os.killpg(p.pid, signal.SIGTERM)
--- play.sh
+++ play.sh
@@ -1,1 +1,3 @@
-echo "tx 14:44:44" | cec-client -s -d 1
+device=`cat ./device`
+device=${device:=4}
+echo "tx 1$device:44:44" | cec-client -s -d 1
