ddd64baa90b40d7f15540b5e7bc8744576e38a5a
J. Longman <jlongman@gmail.com>
2017-04-25 23:12:24 +0000

    add retry and isolate bash scripts

--- button.py
+++ button.py
@@ -6,25 +6,44 @@ import signal
 
 pin = 4
 
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 alternate = False
 
 
-# scan for the chromecast and save it's logical address
-with open("/home/pi/chromecast-button/device", "w") as f:
-  device = 4
+def scan():
   scan = subprocess.Popen("echo scan | cec-client -s -d 1", stdout=subprocess.PIPE, shell=True)
+  device = -1
+  found_device = device
   for line in scan.stdout:
      line = line.lower()
      if line.find("device #") > -1:
           info = line.split()
-          device = info[1][1:-1]
+          found_device = info[1][1:-1]
      if line.find("chromecast") > -1:
-          print(str(device))
+          device = found_device
           break
+  return device
+
+# scan for the chromecast and save it's logical address
+with open("/home/pi/chromecast-button/device", "w") as f:
+  device = "4"
+  try:
+     print("Scanning for ChromeCast...")
+     found_device = scan()
+     if (found_device < 0):
+        print("Chromecast not active, retry")
+        time.sleep(1)
+        found_device = scan()
+        if found_device < 0:
+           print("Chromecast not active, use 4")
+           found_device = 4
+  except:
+     print("Chromecast error, use 4")
+     found_device = 4
+  device = str(found_device)
+  print("Using " + device)
   f.write(device)
   f.close
 
@@ -33,10 +52,10 @@ while True:
 	if input_state == False:
 		if alternate:
 			p = subprocess.Popen("/home/pi/chromecast-button/play.sh", shell=True, preexec_fn=os.setsid)
-			print("play")
+#			print("play")
 		else:	
 			p = subprocess.Popen("/home/pi/chromecast-button/pause.sh", shell=True, preexec_fn=os.setsid)
-			print("pause")
+#			print("pause")
 		time.sleep(2)
 		os.killpg(p.pid, signal.SIGTERM)
 		alternate = not alternate
--- play.sh
+++ play.sh
@@ -1,3 +1,21 @@
-device=`cat ./device`
+#!/bin/bash
+
+device=`cat /home/pi/chromecast-button/device`
 device=${device:=4}
-echo "tx 1$device:44:44" | cec-client -s -d 1
+
+
+you=`basename "$0"`
+case $you in
+stop*)
+  echo "tx 1$device:42:03" | cec-client -s -d 1
+  echo stop
+  ;;
+pause*)
+  echo "tx 1$device:44:46" | cec-client -s -d 1
+  echo bar
+  ;;
+*)
+  echo "tx 1$device:44:44" | cec-client -s -d 1
+  echo play
+  ;;
+esac
